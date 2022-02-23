5816542bc83161258994517d94dbbff1d18a540f
Adam Barnes <ambarnes@gmail.com>
2022-02-23 14:03:24 +0000

    Banner typos

--- src/common/retro/saa5050/saa5050.vhd
+++ src/common/retro/saa5050/saa5050.vhd
@@ -1,13 +1,13 @@
 --------------------------------------------------------------------------------
--- saa5050.vhd                                                             --
+-- saa5050.vhd                                                                --
 -- SAA5050 compatible teletext character generator.                           --
 --------------------------------------------------------------------------------
 -- (C) Copyright 2022 Adam Barnes <ambarnes@gmail.com>                        --
 -- This file is part of The Tyto Project. The Tyto Project is free software:  --
--- you can rechr_d1stribute it and/or mochr_d1fy it under the terms of the GNU Lesser --
+-- you can redistribute it and/or modify it under the terms of the GNU Lesser --
 -- General Public License as published by the Free Software Foundation,       --
 -- either version 3 of the License, or (at your option) any later version.    --
--- The Tyto Project is chr_d1stributed in the hope that it will be useful, but    --
+-- The Tyto Project is distributed in the hope that it will be useful, but    --
 -- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
 -- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
 -- License for more details. You should have received a copy of the GNU       --
