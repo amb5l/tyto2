dc7d8ecc53346a692d6edbfc47b9af695354d347
Adam Barnes <ambarnes@gmail.com>
2022-11-10 19:25:18 +0000

    Change from excluding to specifying builds

--- build/make_all.sh
+++ build/make_all.sh
@@ -1,17 +1,10 @@
 # do a clean build of all designs
 # TODO: add Linux support
 
-# workarounds for Xilinx GNU tool issues
-SLEEP=/usr/bin/sleep
+# workaround for Xilinx GNU tool issues
+alias sleep="/usr/bin/sleep"
 
 set -o errexit
-
-# incomplete designs
-exlusions=("bpp_digilent_nexys_video" "bpp_qmtech_wukong")
-
-dirs=$(find . -maxdepth 2 -mindepth 2 -type d)
-base_dir=$(pwd)
-find . \( -name 'finished' -or -name '*.bit' -or -name '*.sof' \) -type f -exec rm -f {} +
 
 # check for executables
 printf "\nChecking Vivado...\n"
@@ -21,17 +14,29 @@ cmd.exe /C "xsct -eval \"puts [version]\""
 printf "\nChecking Quartus...\n"
 cmd.exe /C "quartus_sh --version"
 
+# build array of directories
+designs=( \
+	'hdmi_tpg' \
+	'np6532_poc' \
+	)
+makefiles=($(find ${designs[@]} -name makefile -type f))
+dirs=()
+for makefile in ${makefiles[@]}; do
+	dirs+=($(dirname $makefile))
+done
+base_dir=$(pwd)
+
 # spawn all builds
 printf "\nspawning builds...\n"
 num_builds=0
-for dir in $dirs; do
-	if [[ ! " ${exlusions[*]} " =~ " $(basename ${dir}) " ]]; then
-		cd $dir
-		# VIVADO_JOBS=1 reduces/eliminates Vivado hang on exit (following wait_on_run)
-		cmd.exe /C "start cmd.exe /C \"pwd & make clean & make VIVADO_JOBS=1 & touch finished & if exist *.bit exit & if exist *.sof exit & pause\""
-		cd $base_dir
-		num_builds=$((num_builds+1))
-	fi;
+for dir in ${dirs[@]}; do
+	cd $dir
+	echo $(pwd)
+	rm -f finished
+	# VIVADO_JOBS=1 reduces/eliminates Vivado hang on exit (following wait_on_run)
+	cmd.exe /C "start cmd.exe /C \"pwd & make clean & rm -f *.bit & rm -f *.sof & set VIVADO_JOBS=1 & make & touch finished & if exist *.bit exit & if exist *.sof exit & pause\""
+	cd $base_dir
+	num_builds=$((num_builds+1))
 done
 
 # wait for all builds to finish
@@ -42,7 +47,7 @@ start_time=$(date +%s)
 next=$(( $interval+$start_time ))
 while (( $finished != $num_builds ))
 do
-	while (( $(date +%s) < next )) ; do $SLEEP 1 ; done
+	while (( $(date +%s) < next )) ; do sleep 1 ; done
 	finished=$(( "$(wc -w <<< $(find . -name finished -type f))" ))
 	now=$(date +%s)
 	printf "time: %ds   finished: %d    \r" $(( $now-$start_time )) $finished
@@ -52,16 +57,14 @@ printf "\nall finished\n\n" $count
 
 # check results
 failures=0
-for dir in $dirs; do
-	if [[ ! " ${exlusions[*]} " =~ " $(basename ${dir}) " ]]; then
-		cd $dir
-		bin=$(find . \( -name '*.bit' -or -name '*.sof' \) -type f)
-		if [ -z $bin ]; then
-			printf "failed: %s\n" $dir
-			failures=$((failures+1))
-		fi
-		cd $base_dir
-	fi;
+for dir in ${dirs[@]}; do
+	cd $dir
+	bin=$(find . \( -name '*.bit' -or -name '*.sof' \) -type f)
+	if [ -z $bin ]; then
+		printf "failed: %s\n" $dir
+		failures=$((failures+1))
+	fi
+	cd $base_dir
 done
 if (( failures )); then
 	printf "%d failures\n" $failures
