fc52cb076fe7e5438e739907a590252329341d93
Adam Barnes <ambarnes@gmail.com>
2023-05-30 14:04:25 +0100

    Comment

--- src/common/axi/axi4_pkg.vhd
+++ src/common/axi/axi4_pkg.vhd
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- axi4_pkg.vhd                                                               --
--- AXI4 type definitions.                                                     --
+-- AXI4 types, constants and functions.                                       --
 --------------------------------------------------------------------------------
 -- (C) Copyright 2023 Adam Barnes <ambarnes@gmail.com>                        --
 -- This file is part of The Tyto Project. The Tyto Project is free software:  --
