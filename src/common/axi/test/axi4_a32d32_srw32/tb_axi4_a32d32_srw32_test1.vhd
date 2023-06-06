c69f12cdffb08bd0e09d651a6ca880f98911995e
Adam Barnes <ambarnes@gmail.com>
2023-06-06 16:40:43 +0100

    Tidying

--- src/common/axi/test/axi4_a32d32_srw32/tb_axi4_a32d32_srw32_test1.vhd
+++ src/common/axi/test/axi4_a32d32_srw32/tb_axi4_a32d32_srw32_test1.vhd
@@ -1,3 +1,20 @@
+--------------------------------------------------------------------------------
+-- tb_axi4_a32d32_srw32_test1.vhd                                             --
+-- OSVVM based testbench for axi4_a32d32_srw32.vhd - test 1                   --
+--------------------------------------------------------------------------------
+-- (C) Copyright 2023 Adam Barnes <ambarnes@gmail.com>                        --
+-- This file is part of The Tyto Project. The Tyto Project is free software:  --
+-- you can redistribute it and/or modify it under the terms of the GNU Lesser --
+-- General Public License as published by the Free Software Foundation,       --
+-- either version 3 of the License, or (at your option) any later version.    --
+-- The Tyto Project is distributed in the hope that it will be useful, but    --
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
+-- License for more details. You should have received a copy of the GNU       --
+-- Lesser General Public License along with The Tyto Project. If not, see     --
+-- https://www.gnu.org/licenses/.                                             --
+--------------------------------------------------------------------------------
+
 architecture tb_axi4_a32d32_srw32_test1 of TestCtrl is
 
   constant TestName : string := "tb_axi4_a32d32_srw32_test1";
