0634da29e62669d5fd4f9330e899681d47962f14
Adam Barnes <ambarnes@gmail.com>
2024-05-26 17:18:40 +0100

    Title block

--- src/common/ethernet/test/tb_memac_fe.vhd
+++ src/common/ethernet/test/tb_memac_fe.vhd
@@ -1,4 +1,19 @@
 --------------------------------------------------------------------------------
+-- tb_memac_fe.vhd                                                            --
+-- Testbench for memac_tx_fe and memac_rx_fe.                                 --
+--------------------------------------------------------------------------------
+-- (C) Copyright 2023 Adam Barnes <ambarnes@gmail.com>                        --
+-- This file is part of The Tyto Project. The Tyto Project is free software:  --
+-- you can redistribute it and/or modify it under the terms of the GNU Lesser --
+-- General Public License as published by the Free Software Foundation,       --
+-- either version 3 of the License, or (at your option) any later version.    --
+-- The Tyto Project is distributed in the hope that it will be useful, but    --
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
+-- License for more details. You should have received a copy of the GNU       --
+-- Lesser General Public License along with The Tyto Project. If not, see     --
+-- https://www.gnu.org/licenses/.                                             --
+--------------------------------------------------------------------------------
 -- definitions required for generic package instances
 
 use work.memac_util_pkg.all;
