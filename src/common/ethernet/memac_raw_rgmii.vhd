0b77e7b46e064c7da3ca6d082a7e8a8a22ac5713
Adam Barnes <ambarnes@gmail.com>
2024-06-13 18:29:06 +0100

    Title block

--- src/common/ethernet/memac_raw_rgmii.vhd
+++ src/common/ethernet/memac_raw_rgmii.vhd
@@ -1,4 +1,19 @@
 --------------------------------------------------------------------------------
+-- memac_raw_rgmii.vhd                                                        --
+-- Modular Ethernet MAC: raw system interface, RGMII PHY interface.           --
+--------------------------------------------------------------------------------
+-- (C) Copyright 2024 Adam Barnes <ambarnes@gmail.com>                        --
+-- This file is part of The Tyto Project. The Tyto Project is free software:  --
+-- you can redistribute it and/or modify it under the terms of the GNU Lesser --
+-- General Public License as published by the Free Software Foundation,       --
+-- either version 3 of the License, or (at your option) any later version.    --
+-- The Tyto Project is distributed in the hope that it will be useful, but    --
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
+-- License for more details. You should have received a copy of the GNU       --
+-- Lesser General Public License along with The Tyto Project. If not, see     --
+-- https://www.gnu.org/licenses/.                                             --
+--------------------------------------------------------------------------------
 
 use work.memac_pkg.all;
 
