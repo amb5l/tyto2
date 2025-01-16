46bca01c8eb193d1350b6e37ff759e8dbf20634c
Adam Barnes <ambarnes@gmail.com>
2025-01-16 22:27:31 +0000

    Remove prng
    Duplicate of sim pkg.

--- src/common/ethernet/memac_util_pkg.vhd
+++ src/common/ethernet/memac_util_pkg.vhd
@@ -24,15 +24,6 @@ package memac_util_pkg is
 
   constant kByte : integer := 1024;
 
-  type prng_t is protected
-    procedure rand_seed(s1, s2 : in integer);
-    impure function rand_real return real;
-    impure function rand_int(min, max : in integer) return integer;
-    impure function rand_slv(min, max, width : in integer) return std_ulogic_vector;
-  end protected prng_t;
-
-  shared variable prng : prng_t;
-
   function ternary( b : boolean; t, f : integer ) return integer;
   function bool2sl( b : boolean ) return std_ulogic;
   function bin2gray( bin : std_ulogic_vector ) return std_ulogic_vector;
@@ -48,33 +39,6 @@ end package memac_util_pkg;
 --------------------------------------------------------------------------------
 
 package body memac_util_pkg is
-
-  type prng_t is protected body
-    variable seed1, seed2 : integer := 0;
-    procedure rand_seed(s1, s2 : in integer) is
-    begin
-      seed1 := s1;
-      seed2 := s2;
-    end procedure rand_seed;
-    impure function rand_real return real is
-      variable r : real;
-    begin
-      uniform(seed1, seed2, r);
-      return r;
-    end function rand_real;
-    impure function rand_int(min, max : in integer) return integer is
-      variable r : real;
-    begin
-      uniform(seed1, seed2, r);
-      return integer(r * real(max - min) + real(min));
-    end function rand_int;
-    impure function rand_slv(min, max, width : in integer) return std_ulogic_vector is
-      variable r : real;
-    begin
-      uniform(seed1, seed2, r);
-      return std_ulogic_vector(to_unsigned(integer(r * real(max - min) + real(min)), width));
-    end function rand_slv;
-  end protected body prng_t;
 
   function ternary( b : boolean; t, f : integer ) return integer is
   begin
